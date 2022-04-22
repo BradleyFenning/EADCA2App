e2ba1a106667cfbffb68f9a6d3506bbc756af065
BradleyFenning <bradleyfenning@gmail.com>
2022-04-22 18:07:26 +0100

    App main request

---
+++ app/src/main/java/com/example/eadca2app/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.eadca2app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    EditText dataInput;
+    ListView bookList;
+    final WeatherDataService weatherDataService = new WeatherDataService(MainActivity.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button1 = findViewById(R.id.button1);
+        dataInput = findViewById(R.id.userInput);
+        bookList = findViewById(R.id.bookList);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                weatherDataService.getCityID(dataInput.getText().toString(), new WeatherDataService.VolleyResponseListener() {
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(MainActivity.this, "Something Wrong" , Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(String cityID) {
+                        Toast.makeText(MainActivity.this, "Returned an ID of " + cityID, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
+}
